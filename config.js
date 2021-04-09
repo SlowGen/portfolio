51bdb9572f4c6d87b0a3f1c1d8ae30e38e96afbc
slowgen <kalibobailey@gmail.com>
2021-04-09 14:56:45 -0400

    Fix: moved navicon

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'public/logo.png', //favicon
+  manifestIcon: 'src/assets/img/logo.png', //favicon
   heading: 'kali.nyc',
   subHeading: 'Software Engineer',
   // social
