cf9bb6da028eceaac570a1d863e0c358335b244b
slowgen <kalibobailey@gmail.com>
2021-04-09 14:43:37 -0400

    Feat: fixed msg redirect, added thank you message

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import config from '../../config';
+
 export default function Footer() {
+  let [isSent, setIsSent] = useState(false);
+  function displayThankYou() {
+    setIsSent(true)
+    setTimeout(function() {
+      setIsSent(false)
+      document.getElementById("name").value=''
+      document.getElementById("email").value=''
+      document.getElementById("message").value=''
+    }, 5000);
+  }
   return (
     <section id="footer">
       <div className="inner">
@@ -8,7 +19,11 @@ export default function Footer() {
         <p>
           Have a question about one of my projects or looking for someone to help on your own project? Reach out to me here!
         </p>
-        <form method="post" action="https://formspree.io/f/xoqpbajy">
+        {isSent ? 
+        <div>
+          <h1>Thanks for reaching out!!</h1><br/><br/><br/><br/>
+        </div> :
+        <form method="post" onSubmit={() => displayThankYou()} action="https://formspree.io/f/xoqpbajy" target='popup'>
           <div className="fields">
             <div className="field">
               <label htmlFor="name">Name</label>
@@ -26,9 +41,11 @@ export default function Footer() {
           <ul className="actions">
             <li>
               <input type="submit" value="Send Message" />
+              
             </li>
           </ul>
         </form>
+        }
         <ul className="contact">
           <li className="fa-home">{config.address}</li>
 
@@ -45,6 +62,7 @@ export default function Footer() {
         </ul>
         <ul className="copyright">
           <li>&copy; 2021 Slowgen. All rights reserved.</li>
+          <iframe name="popup" title="SlowGen" height={0} width={0}></iframe>
           <li>
           </li>
         </ul>
